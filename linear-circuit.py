5d4b9c961bc38e6d0546940f6afc632b93c3602d
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-02-11 00:27:11 +0300

    W?

--- linear-circuit.py
+++ linear-circuit.py
@@ -19,7 +19,7 @@ if __name__ ==  "__main__":
         e[1] = int(e[1])
         e[2] = e[2].lower()
         e[3] = float(e[3])
-        if   e[2] == 'u':
+        if e[2] == 'u':
             vsrcCount += 1
         nodeSet.add(e[0])
         nodeSet.add(e[1])
@@ -32,31 +32,43 @@ if __name__ ==  "__main__":
     # S O L V E
 
     # AX = Z, X - unknowns
-    unknownCount = nodeCount+vsrcCount
+    unknownCount = (nodeCount-1)+vsrcCount
     A = np.zeros((unknownCount, unknownCount))
     Z = np.zeros((unknownCount, 1))
-    
+
     currentVsrc = 0
     for e in circ:
-        if e[2] == 'r':
-            A[e[0]][e[0]] += 1/e[3]
-            A[e[1]][e[1]] += 1/e[3]
-            A[e[0]][e[1]] -= 1/e[3]
-            A[e[1]][e[0]] -= 1/e[3]
-        elif e[2] == 'u':
-            i = nodeCount+currentVsrc
-            A[i][e[0]] = -1
-            A[e[0]][i] = -1
-            A[i][e[1]] = +1
-            A[e[1]][i] = +1
+        basicFlag = -1
+        if e[0] == nodeCount-1: basicFlag = 0
+        if e[1] == nodeCount-1: basicFlag = 1
+        if e[2] == 'u':
+            i = nodeCount-1+currentVsrc
+            if basicFlag != 0:
+                A[i][e[0]] = -1
+                A[e[0]][i] = -1
+            if basicFlag != 1:
+                A[i][e[1]] = +1
+                A[e[1]][i] = +1
             Z[i] = e[3]
             currentVsrc += 1
-        elif e[2] == 'i':
-            Z[e[0]] = -e[3]
-            Z[e[1]] = +e[3]
+            continue
+        if e[2] == 'r':
+            if basicFlag != 0: A[e[0]][e[0]] += 1/e[3]
+            if basicFlag != 1: A[e[1]][e[1]] += 1/e[3]
+            if basicFlag == -1:
+                A[e[0]][e[1]] -= 1/e[3]
+                A[e[1]][e[0]] -= 1/e[3]
+            continue
+        if e[2] == 'i' and basicFlag == -1:
+            Z[e[0]] = +e[3]
+            Z[e[1]] = -e[3]
+            continue
 
+    print(A)
+    print(Z)
     try:
         X = np.linalg.solve(A, Z)
         print(X.transpose())
     except:
         print('Ill-formed matrix.')
+
